61a7649d9fa83aa4ae8a845536bd42fef05b9afe
Klas Hermodsson <klas@khermodsson.dev>
2025-03-23 19:05:30 +0100

    no message

--- ansible/install_ansible.sh
+++ ansible/install_ansible.sh
@@ -3,5 +3,12 @@
 # CLoudShell saves the files you have created but not installed packages and software
 # so you may need to run this after being inactive on CloudShell
 
+echo "⬇️ Installerar Ansible..."
+
 sudo dnf update -y
 sudo dnf install -y ansible
+
+
+echo "⬇️ Installerar pumssql (används för att skapa tabell)..."
+
+pip install pymssql
