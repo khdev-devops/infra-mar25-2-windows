dc9da6d80c3251733be4ddb1679c5964169e5d6e
Klas Hermodsson <klas@khermodsson.dev>
2025-03-25 14:23:40 +0100

    Fixed missing module to do winrm ping

--- ansible/install_ansible.sh
+++ ansible/install_ansible.sh
@@ -3,7 +3,11 @@
 # CLoudShell saves the files you have created but not installed packages and software
 # so you may need to run this after being inactive on CloudShell
 
+echo "Installerar ansible..."
+
 sudo dnf update -y
 sudo dnf install -y ansible
+
+echo "Installerar Python WinRm modul..."
 
 pip install pywinrm
